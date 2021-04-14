e1f8309a4947fe441c02dfbf95e05002523c0a7c
itsme.fui <48958743+tranvangiahuy1999@users.noreply.github.com>
2021-04-14 20:39:43 +0700

    notificate update

--- src/components/admin/notificate-all.js
+++ src/components/admin/notificate-all.js
@@ -1,6 +1,10 @@
 import React, {useState} from 'react'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { AiFillNotification } from "react-icons/ai";
+import { BiCalendar } from "react-icons/bi";
+
+const mm = require('micromatch');
 
 const NotiPage = (props) => {
     const [searchNotiTitle, setSearchNotiTitle] = useState('')
@@ -8,39 +12,61 @@ const NotiPage = (props) => {
     const [searchFalcuty, setSearchFalcuty] = useState('')
     const [startDate, setStartDate] = useState(new Date());
 
+    function FalcutyFilter(){
+        
+    }
+
     return(
-        <div style={{justifyContent:'center', padding:'10px'}}>
-            <div style={{color:'gray', backgroundColor:'white', textAlign:'center'}}>
-            NOTIFICATON
-            </div>
-            <div style={{backgroundColor:'white', margin: '5px', padding:'5px'}}>
-                <div className='noti-filter'>
+        <div style={{justifyContent:'center', padding:'15px'}}>
+            <h5 style={{color:'gray', backgroundColor:'white', textAlign:'center', padding:'5px'}}>
+                NOTIFICATON<AiFillNotification style={{marginLeft:'5px'}} size="23px" color="gray"/>
+            </h5>
+            <div className='noti-filter' style={{backgroundColor:'white', margin: '2px', padding:'4px'}}>
+                <div>
                     <div className='row col-md-12 col-sm-12 p-2'>
-                        <div class="col-md-5 col-sm-5">
-                            <input style={{padding: '4px', width:'100%'}} placeholder='Search by notificate tittle' value={searchNotiTitle} onChange={v => setSearchNotiTitle(v.target.value)} ></input>    
+                        <div class="form-group col-md-5 col-sm-5">
+                            <label>Search by Notificate tittle</label>
+                            <input className='form-control' style={{padding: '4px', width:'100%'}} placeholder='notificate tittle' value={searchNotiTitle} onChange={v => setSearchNotiTitle(v.target.value)} ></input>    
                         </div>
-                        <div class="col-md-5 col-sm-5">
-                            <input style={{padding: '4px', width:'100%'}} placeholder='Search by notificate content' value={searchNotiContent} onChange={v => setSearchNotiContent(v.target.value)}></input>    
+                        <div class="form-group col-md-5 col-sm-5">
+                            <label>Search by Notificate content</label>
+                            <input className='form-control' style={{padding: '4px', width:'100%'}} placeholder='notificate content' value={searchNotiContent} onChange={v => setSearchNotiContent(v.target.value)}></input>    
                         </div>
-                        <div class="col-md-2 col-sm-2">
-                            <input style={{padding: '4px', width:'100%'}} placeholder='Search by falcuty' value={searchFalcuty} onChange={v => setSearchFalcuty(v.target.value)}></input>
+                        <div class="form-group col-md-2 col-sm-2">
+                            <label>Search by Falcuty</label>
+                            <input className='form-control' style={{padding: '4px', width:'100%'}} placeholder='falcuty' value={searchFalcuty} onChange={v => setSearchFalcuty(v.target.value)}></input>
                         </div>
                     </div>
-                    <div className='row col-md-12 col-sm-12 p-2'>
-                        <div class="col-md-7 col-sm-7">
-                            <DatePicker style={{padding: '4px', width:'100%'}} selected={startDate} onChange={date => setStartDate(date)} />
+                    <div className=' row col-md-12 col-sm-12 p-2'>
+                        <div class="form-group col-md-5 col-sm-5">
+                            <label className='pr-4'>Search by Date<BiCalendar style={{marginLeft:'6px'}} size='20px' color='black'></BiCalendar></label>
+                            <DatePicker className='form-control' selected={startDate} onChange={date => setStartDate(date)} />
                         </div>
-                        <div class="col-md-3 col-sm-3">
-
-                        </div>
+                        <div class="form-check col-md-3 col-sm-3">                        
+                            <div style={{width:'50%'}}>
+                                <input className="form-check-input" type="checkbox" />
+                                <label className="form-check-label">
+                                    Unread
+                                </label>
+                            </div>
+                            <div style={{width:'50%'}}>                        
+                                <input className="form-check-input" type="checkbox" />
+                                <label className="form-check-label">
+                                    Important
+                                </label>
+                            </div>
+                        </div>                        
                         <div class="col-md-2 col-sm-2">
-                            
-                        </div>    
+                            <button className='btn btn-danger'>Delete All</button>            
+                        </div> 
+                        <div class="col-md-2 col-sm-2">                            
+                            <button className='btn btn-primary p'>Search</button>
+                        </div> 
                     </div>
                 <div className='noti-list'>
 
                 </div>
-                </div>
+            </div>
             </div>
         </div>
     )
