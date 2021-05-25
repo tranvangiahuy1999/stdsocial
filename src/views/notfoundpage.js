1c02f70b98236ba114a2d96588067b35904fa4f5
Gia Huy <tranvangiahuy1999@gmail.com>
2021-05-25 15:40:20 +0700

    Update notfoundpage.js

--- src/views/notfoundpage.js
+++ src/views/notfoundpage.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { Result, Button } from 'antd';
 import {useHistory} from 'react-router-dom'
+import { useAlert } from 'react-alert';
 
 const NotFoundComponent = props => {
     const history = useHistory()
+    const alert = useAlert()
+    useEffect(() => {
+        alert.show('Redirect to login page...', {
+            type: 'success'
+        })
+        setTimeout(()=> {
+            history.push('/login')
+        }, 3000)
+    } , [])
     return (
         <Result
             status="404"
